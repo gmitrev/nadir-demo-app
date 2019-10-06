aa7690c450f5c235ca1990480cd45fe0382b1a70
Georgi Mitrev <gvmitrev@gmail.com>
2019-10-06 20:29:56 +0300

    Indeed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   # validate :body, presence: true
   validates :body, presence: true
+
+  before_save do
+    raise 'Nobody Expects The Spanish Inquisition' if rand > 0.5
+  end
 end
